e15502db1c061426e36756c5a936f0a2335ba444
watertransparent <freelancer3011@gmail.com>
2021-12-27 16:00:10 +0300

    Laba6 added

---
+++ data.sql
@@ -0,0 +1,14 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com'),
+('Иван', 'Александрович', 'Пират', 'pirat@my.example.com'),
+('Ракислав', 'Антонович', 'Ракитин', 'rakitin@my.example.com'),
+('Дмитрий', 'Дмитриевич', 'Мустафаев', 'mustafaev@my.example.com'),
+('Даниил', 'Генадьевич', 'Ильин', 'ilin@my.example.com'),
+('Стас', 'Суфьянович', 'Мак', 'mac@my.example.com'),
+('Виктория', 'Викторовна', 'Пельмень', 'pelmen@my.example.com'),
+('Дмитрий', 'Андреевич', 'Двор', 'dvor@my.example.com'),
+('Александра', 'Андреевна', 'Шуткина', 'shutkina@my.example.com'),
+('Артем', 'Артемович', 'Артемов', 'artemov@my.example.com'),
+('Александр', 'Дмитриевич', 'Иванов', 'ivancov@my.example.com');
---
+++ schema_db.sql
@@ -0,0 +1,7 @@
+create table usr (
+	id bigserial not null,
+	email varchar(64) not null,
+	first_name varchar(64) not null,
+	last_name varchar(64) not null,
+	middle_name varchar(64) not null
+);
