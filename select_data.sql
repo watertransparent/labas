abec882e6ede8755144f61046991e195ea630440
watertransparent <freelancer3011@gmail.com>
2022-02-07 00:51:40 +0300

    lab15-16

--- select_data.sql
+++ select_data.sql
@@ -15,3 +15,13 @@ select u.first_name, u.middle_name, u.last_name, a.zip_code, a.city, a.street, a
  a.flat from usr u inner join address a on u.id=a.user_id;
 select u.first_name, u.middle_name, u.last_name, a.zip_code, a.city, a.street, a.building,
  a.flat from usr u left join address a on u.id=a.user_id;
+ create view dep AS
+select m.first_name, m.middle_name, m.last_name, m.e_mail, d.name
+from university_member um join usr m on um.user_id=m.id join department d on um.department_id=d.id;
+create view grop AS
+select m.first_name, m.middle_name, m.last_name, m.e_mail, g.name
+from group_member gm join university_member um on gm.university_member_id=um.user_id
+join groups g on gm.group_id=g.id join usr m on um.user_id=m.id; 
+create view pos_usr AS
+select m.first_name, m.middle_name, m.last_name, m.e_mail, p.name
+from university_member um left join usr m on um.user_id=m.id join position p on um.position_id=p.id; 
